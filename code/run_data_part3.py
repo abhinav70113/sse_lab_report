7d31984b6f147aba0e006d93a60ee8e5f30be0ad
abhinav70113 <bhnvtyagi@gmail.com>
2021-12-12 23:37:15 +0100

    restructured the project

--- code/run_data_part3.py
+++ code/run_data_part3.py
@@ -6,7 +6,7 @@ import mesa_reader
 
 mpl.rcParams.update(mpl.rcParamsDefault)
 #h = mesa_reader.MesaData('/Users/abhinavtyagi/Desktop/scp_data/LOGS/history.data')
-df_data = pd.read_csv('/df_1Msun_history.csv')
+df_data = pd.read_csv('/Users/abhinavtyagi/PycharmProjects/pythonProject/CSVs/df_1Msun_history.csv')
 df = df_data[df_data['star_age']<20e15]
 
 plt.figure()
@@ -22,6 +22,7 @@ plt.colorbar(label = 'Age of the star [yrs]')
 plt.scatter(620,400000000, marker='*', color = 'red', s = 150, label='sun')
 
 plt.legend()
-#plt.savefig('HRplot.pdf')
 plt.show()
+plt.savefig('/Users/abhinavtyagi/PycharmProjects/pythonProject/images/Model_timestep.png')
+
 
--- code/run_data_part4.py
+++ code/run_data_part4.py
@@ -6,7 +6,7 @@ import mesa_reader
 
 mpl.rcParams.update(mpl.rcParamsDefault)
 #h = mesa_reader.MesaData('/Users/abhinavtyagi/Desktop/scp_data/LOGS/history.data')
-df_data = pd.read_csv('/df_profile91.csv')
+df_data = pd.read_csv('/Users/abhinavtyagi/PycharmProjects/pythonProject/CSVs/df_profile91.csv')
 df = df_data
 #df = df_data[df_data['star_age']<20e15]
 df['radius_color'] = df['radius']*100000
@@ -14,7 +14,7 @@ df['radius_color'] = df['radius_color'].astype(int)
 
 
 plt.figure()
-zone = df['zone']
+Zone = df['zone']
 x_frac = df['x_mass_fraction_H']
 y_frac = df['y_mass_fraction_He']
 z_frac = df['z_mass_fraction_metals']
@@ -30,6 +30,6 @@ plt.colorbar(label = 'radial coordinate * 100000')
 #plt.scatter(620,400000000, marker='*', color = 'red', s = 150, label='sun')
 
 plt.legend()
-#plt.savefig('HRplot.pdf')
+plt.savefig('/Users/abhinavtyagi/PycharmProjects/pythonProject/images/composition.png')
 plt.show()
 
