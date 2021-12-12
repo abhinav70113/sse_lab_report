e7560adb6b4b845a3b0305bf68b12746cdf3d781
abhinav Tyagi <abhinavtyagi@abhinavs-MacBook-Air.local>
2021-12-12 22:08:16 +0100

    Initial commit

---
+++ read_history.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('/Users/abhinavtyagi/Desktop/scp_data/LOGS/history.data', 'r') as txt_file:
+    data = txt_file.readlines()
+
+columns = data[5].split()
+df_data = pd.DataFrame(columns = columns)
+
+for i in range(5,len(data)):
+    words = data[i].split()
+    df_data.loc[i-5] = words
+df_data = df_data[1:]
+
+df_data = df_data.astype(float)
+df_data['star_age'] = df_data['star_age'].astype(int)
+df_data.to_csv('df_1Msun_history.csv')
+
